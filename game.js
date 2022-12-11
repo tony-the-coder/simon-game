cc1281c67e137102d2819a5b72051185c97cecfa
Tony <tonythecoder@outlook.com>
2022-12-10 21:17:33 -0500

    First attempt at sound

---
+++ game.js
@@ -0,0 +1,21 @@
+var buttonColors = ["red", "blue", "green", "yellow"]
+var gamePattern = []
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random() * 4)
+    var randomChosenColour = buttonColors[randomNumber]
+    gamePattern.push(randomChosenColour)
+
+    $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3")
+    audio.play();
+    // return randomChosenColour
+}
+
+
+
+// var randomChosenColour = buttonColors[randomNumber]
+
+console.log((nextSequence()
+))
